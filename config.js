3ed91fa1855843bfa5feb3b37945ba741594648a
oshadaX-Wolf <153474645+oshadaX-Wolf@users.noreply.github.com>
2025-01-02 22:16:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ function convertToBool(text, fault = "true") {
 module.exports = {
     SESSION_ID:
         process.env.SESSION_ID ||
-        "XYUliSIL#N4k8L_32rdS9EggHk8hi5EfdMPKIyk_FZkSYSY0izjQ",
+        "",
     MONGODB:
         process.env.MONGODB ||
-        "mongodb+srv://Oshada2005:Oshada2005@cluster2005.kuyvoqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2005",
+        "",
 };
